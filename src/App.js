6fce04b2438e9b8179fdd7b5955642b371d8d106
djtharun <djtharun2006@gmail.com>
2025-05-26 20:46:29 +0530

    continuing removal of widget feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainApp from './pages/Home';
-import TimerWidget from './components/TimerWidget';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainApp />} />
-        <Route path="/widget" element={<TimerWidget />} />
       </Routes>
     </BrowserRouter>
   );
